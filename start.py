24d373c14585ceea829c040712c8f85a66aced37
Biobaku Oluwole <wolexzo007@gmail.com>
2025-07-25 13:40:16 +0100

    First code

---
+++ start.py
@@ -0,0 +1,86 @@
+""" This is a comment
+ This is a comment
+ This is a comment
+ This is a comment
+ This is a comment
+ This is a comment
+"""
+#print("I am new to python programming!");
+
+x = "Biobaku";
+y = ["Kunle","Timothy","Ibrahim","Tajudeen","Muyiwa"];
+yc = [34,56,76,12,78];
+
+#fullname = x + " " + y;
+
+#print(fullname);
+
+#data_type = type(y);
+
+#print(data_type);
+
+holdtogether = y[0] + " and " + y[4] + " are both best friends";
+
+#print(holdtogether);
+
+#print(yc[2]);
+
+#x = input("Enter the value of x?") ;
+#y = input("Enter the value of y?") ;
+
+#z = 2*int(x) + int(y);
+#e = 7 * x;
+#print(e);
+
+"""
+name = input("Please enter your name:");
+
+takeCGP = input("Please enter your cgp:");
+
+cgp = float(takeCGP);
+
+if(cgp < 2):
+
+    response = "You failed ";
+
+elif(cgp > 2):
+
+ response = "You passed ";
+
+else:
+   
+   response = "Nothing";
+
+print(name);
+
+print(response);"""
+
+
+askforname = input("Please enter your name:");
+
+if(askforname == "timothy"):
+
+    respondTo = "You are the software mentor";
+
+elif(askforname == "ayo"):
+   
+   respondTo = "You are a cybersecurity student at xelow-gc";
+
+elif(askforname == "wale"):
+   
+    respondTo = "You are a flutter flow mobile developer at xelow-gc";
+
+else:
+   
+    respondTo = "Sorry I could not process your request!";
+
+print("Your fullname is " + askforname.upper());
+
+print(respondTo);
+
+print("Thank you! Bye for now.");
+
+
+
+
+
